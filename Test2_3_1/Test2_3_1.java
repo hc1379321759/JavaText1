2e585cfb6b19febcab61ad0be56c98719c3d113c
hc1379321759 <1379321759@qq.com>
2019-03-20 17:15:02 +0800

    JavaText1

---
+++ Test2_3_1/Test2_3_1.java
@@ -0,0 +1,14 @@
+package com.hc.Test2_3_1;
+
+import java.util.*;
+
+public class Test2_3_1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while (true){
+            System.out.println("请输入：");
+            String str = s.nextLine();
+            System.out.println(str);
+        }
+    }
+}
--- javatest/src/com/hc/Test.java
+++
@@ -1,7 +0,0 @@
-package com.hc;
-
-/**
- * 用于测试
- */
-public class Test {
-}
